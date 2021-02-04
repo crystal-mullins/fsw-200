d9be38e209940c5c3f4f3dc2e6358238338dc2f2
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2021-02-03 19:19:31 -0800

    P

---
+++ week3/joinandsplit.py
@@ -0,0 +1,12 @@
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Jerry, Trey, Bob, Mike, George, Carl']
+print(friends_list)
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+csv = [' ' + a + ' ' for a in csv]
+# for a in csv:
+#     print(' ,')
+# csv = [' {0} '.format(elem) for elem in csv]
+print(csv)
+# lst = ['a', 'b', 'c']
+# lst2 = [' ' + a + ' ' for a in lst]
+# print(lst2)
---
+++ week3/tuple.py
@@ -0,0 +1,6 @@
+John = ("John", "Mayer", "Stranger things 2020", "Rockstar", "Conn")
+
+values = (1,2,3,4,5,6,7,8,9,10)
+
+print(John[0])
+print(values[1:4])
